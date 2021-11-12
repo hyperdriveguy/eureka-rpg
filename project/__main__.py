699848ad9589457c073c8c23d219dc8fdfb691d7
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-11-12 12:00:05 -0700

    add skeleton Window

--- project/__main__.py
+++ project/__main__.py
@@ -1,5 +1,12 @@
-def main(screen):
-    pass
+from arcade import run
+
+from game.constants import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_NAME
+from game.main_window import MainWindow
+
+def main():
+    game = MainWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_NAME)
+    game.setup()
+    run()
 
 if __name__ == "__main__":
     main()
