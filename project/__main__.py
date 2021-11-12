68f36a753c63488cb9afb5c3588ccc1c8c59901d
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-11-12 12:21:46 -0700

    Make it so code format is up to par for pylint. Also reduced required passing score to 7

--- project/__main__.py
+++ project/__main__.py
@@ -1,9 +1,16 @@
+"""Module to run the game.
+"""
+
 from arcade import run
 
 from game.constants import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_NAME
 from game.main_window import MainWindow
 
 def main():
+    """Main function for playing the game.
+
+    This function passes the needed constants to set up the game window.
+    """
     game = MainWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_NAME)
     game.setup()
     run()
