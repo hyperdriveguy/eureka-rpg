42cd56e473bc167f35dea7a13190260df50f4293
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-11-15 00:31:42 -0700

    Start adapting platformer tutorial Added placeholder character, added camera and minimal physics

--- project/game/constants.py
+++ project/game/constants.py
@@ -1,3 +1,10 @@
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 GAME_NAME = "Eureka!"
+
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 2
+TILE_SCALING = 0.5
+
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 5
--- project/game/main_window.py
+++ project/game/main_window.py
@@ -1,4 +1,6 @@
 import arcade
+import random
+from game import constants
 
 class MainWindow(arcade.Window):
     """
@@ -12,15 +14,74 @@ class MainWindow(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        arcade.set_background_color(arcade.color.BROWN_NOSE)
+        # Our Scene Object
+        self.scene = None
+
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        # Our physics engine
+        self.physics_engine = None
+
+        # A Camera that can be used for scrolling the screen
+        self.camera = None
+
+        arcade.set_background_color(arcade.color.DENIM)
 
         # If you have sprite lists, you should create them here,
         # and set them to None
 
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
-        # Create your sprites and sprite lists here
-        pass
+
+        # Initialize Scene
+        self.scene = arcade.Scene()
+
+        # Create the Sprite lists
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
+
+        # Set up the player, specifically placing it at these coordinates.
+        # image_source = ":resources:images/animated_characters/male_adventurer/maleAdventurer_idle.png"
+        image_source = 'project/assets/placeholder.png'
+        self.player_sprite = arcade.Sprite(image_source, constants.CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 256
+        self.scene.add_sprite("Player", self.player_sprite)
+
+        # Create the ground
+        # This shows using a loop to place multiple sprites horizontally
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", constants.TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.scene.add_sprite("Walls", wall)
+
+        # Put some crates on the ground
+        # This shows using a coordinate list to place sprites
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+        for y in range(96, 5000, 64):
+            coordinate_list.append([32, y])
+            coordinate_list.append([1250, y])
+        for _ in range(100):
+            coordinate_list.append([random.randint(96, 1250), random.randint(0, 5000)])
+
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", constants.TILE_SCALING
+            )
+            wall.position = coordinate
+            self.scene.add_sprite("Walls", wall)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player_sprite, self.scene.get_sprite_list("Walls")
+        )
+
+        # Setup the Camera
+        self.camera = arcade.Camera(self.width, self.height)
+
 
     def on_draw(self):
         """
@@ -33,13 +94,39 @@ class MainWindow(arcade.Window):
 
         # Call draw() on all your sprite lists below
 
+        # Draw our Scene
+        self.scene.draw()
+
+        # Activate our Camera
+        self.camera.use()
+
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+            self.camera.viewport_height / 2
+        )
+
+        # Don't let camera travel past 0
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+
     def on_update(self, delta_time):
         """
         All the logic to move, and the game logic goes here.
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        pass
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
+        # Position the camera
+        self.center_camera_to_player()
 
     def on_key_press(self, key, key_modifiers):
         """
@@ -48,13 +135,29 @@ class MainWindow(arcade.Window):
         For a full list of keys, see:
         https://api.arcade.academy/en/latest/arcade.key.html
         """
-        pass
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = constants.PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -constants.PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -constants.PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = constants.PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, key_modifiers):
         """
         Called whenever the user lets off a previously pressed key.
         """
-        pass
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         """
