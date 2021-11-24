6d141197457cd4e87d21c45d23016016505669df
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-11-24 01:02:51 -0700

    Battle progress snapshot

--- project/game/battle.py
+++ project/game/battle.py
@@ -4,5 +4,20 @@ class Battle(arcade.View):
 
     def __init__(self):
         super().__init__()
+        arcade.set_background_color(arcade.color.WHITE)
     
-    
+    def setup(self):
+        # Setup the Camera
+        self.camera = arcade.Camera(self.window.width, self.window.height)
+
+        # Setup the GUI Camera
+        self.gui_camera = arcade.Camera(self.window.width, self.window.height)
+
+    def on_draw(self):
+        pass
+
+    def on_update(self):
+        pass
+
+    def on_show_view(self):
+        pass
---
+++ project/game/contestant.py
@@ -0,0 +1,7 @@
+import arcade
+from game import constants
+
+class Contestant(arcade.Sprite):
+
+    def __init__(self):
+        pass
