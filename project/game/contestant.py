002ebb1e7563dca1f556f802ed2bad5e8759c208
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-11-26 02:58:45 -0700

    Add some attrs to contestant

--- project/game/contestant.py
+++ project/game/contestant.py
@@ -4,4 +4,16 @@ from game import constants
 class Contestant(arcade.Sprite):
 
     def __init__(self):
-        pass
+        self._scale = constants.CHARACTER_SCALING
+        
+        # Base Battle Stats
+        self._heart_points = 10
+        self._attack = 5
+        self._defense = 5
+        self._skill = 5
+        self._speed = 5
+    
+    def update_animation(self, delta_time: float = 1 / 60):
+        raise ValueError('"update_animation" was properly overridden by child.')
+    
+
