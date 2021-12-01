5b1f021f2fa8db586f05a91fdb9bf4918e9bf623
brynlol <brynlol12@gmail.com>
2021-11-30 21:30:20 -0700

    Revert "Added comments to ui_elements.py"
    This reverts commit dad303b5e8d0389a4688d2bd18dee88534ced8f3.

--- project/game/ui_elements.py
+++ project/game/ui_elements.py
@@ -1,12 +1,7 @@
 import arcade
 
 class Button(arcade.SpriteList):
-    """ Responsible for creating ui buttons 
     
-        Attributes:
-            self._action_name (string): button name
-            self._button_text (text sprite): the button text
-    """
     def __init__(self,
                 text: str,
                 start_x: float,
@@ -20,7 +15,6 @@ class Button(arcade.SpriteList):
                 anchor_y: str = "baseline",
                 rotation: float = 0,
                 ):
-        """ Class Constructor """
         super().__init__()
         
         self._action_name = text
@@ -46,11 +40,8 @@ class Button(arcade.SpriteList):
     
 
 class Selector:
-    """ 
-    Responsible for selecting buttons 
-    """
+    
     def __init__(self, *buttons, orient='main'):
-        """ Class Constructor """
         self._button_actions = {}
         self._button_list = arcade.SpriteList()
         for button in buttons:
@@ -65,12 +56,11 @@ class Selector:
         self._can_select = False
 
     def _add_selector(self):
-        """ Create a rectangle as a selector for the buttons """
         self._selection_box = arcade.create_rectangle(
             self._button_list[self._cur_selection].center_x,
-            self._button_list[self._cur_selection].center_y,
-            self._button_list[self._cur_selection].width * 1.1,
-            self._button_list[self._cur_selection].height * 0.9,
+            self._button_list[self._cur_selection].center_y * 0.78,
+            self._button_list[self._cur_selection].width *1.1,
+            self._button_list[self._cur_selection].height * 0.8,
             arcade.color.BLACK,
             border_width=3,
             filled=False
@@ -78,14 +68,12 @@ class Selector:
         self._selector_list.append(self._selection_box)
     
     def next_button(self):
-        """ Move the selection to the next button """
         self._cur_selection += 1
         if self._cur_selection >= len(self._button_list):
             self._cur_selection = 0
         self._update_selector()
     
     def prev_button(self):
-        """ Move the selection to the previous button """
         self._cur_selection -= 1
         if self._cur_selection < 0:
             self._cur_selection = len(self._button_list) - 1
@@ -95,12 +83,10 @@ class Selector:
         return self._button_actions[self._button_list[self._cur_selection]]
     
     def _update_selector(self):
-        """ Update the selector """
         self._selector_list.remove(self._selection_box)
         self._add_selector()
     
     def draw(self):
-        """ Draw the selector """
         self._button_list.draw()
         if self._can_select:
             self._selector_list.draw()
