7a09b68382756c0b29e218d0faa43913cba0ad4f
Hyperdriveguy <hyperdriveguy@gmail.com>
2021-11-30 19:25:06 -0700

    Comment BattlePlayer

--- project/game/battle_player.py
+++ project/game/battle_player.py
@@ -1,7 +1,19 @@
+"""Contains classes for the player battling.
+"""
+
 import arcade
 from game.contestant import Contestant
 
+
 class BattlePlayer(Contestant):
+    """Class used for the player during battle.
+
+    Inherits: Contestant
+    """
 
     def __init__(self):
-        super().__init__(['project/assets/player_placeholder.png', 'project/assets/player_placeholder_2.png', 'project/assets/player_placeholder_3.png'])
+        """Initialize the contestant and other attributes.
+        """
+        super().__init__(['project/assets/player_placeholder.png',
+                          'project/assets/player_placeholder_2.png',
+                          'project/assets/player_placeholder_3.png'])
