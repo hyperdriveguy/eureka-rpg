3168808ed66fb8e171f9e8258f8bbf98c121cfc7
brynlol <brynlol12@gmail.com>
2021-11-30 19:49:08 -0700

    Added comments to text_box.py

--- project/game/text_box.py
+++ project/game/text_box.py
@@ -3,8 +3,26 @@ import textwrap
 from game import constants
 
 class DrawTextBox:
+    """ Responsible for drawing text boxes
+    
+    Attributes:
+        self.rec_width (int): width of text box
+        self.rec_length (int): length of text box
+        self.text_box_center_x (int): horizontal position of the center point of the text box
+        self.text_box_center_y (int): vertical position of the center point of the text box
+        self.wrapper (TextWrapper): An instance of TextWrapper
+        self.text_list (list): One string split into a list of strings to wrap the text
+        self.text (str): The text to be added to the text box
+        self.cur_line (int): The last line added to the text box
+        self.text_end (bool): Determine if the end of the text has been reached
+        """
 
     def __init__(self, text):
+        """ Class Constructor 
+            Args:
+                self (DrawTextBox): An instance of DrawTextBox
+                text (str): The text to be printed in the text box
+        """
         self.rec_width = 500    
         self.rec_length = 100
         self.text_box_center_x = constants.SCREEN_WIDTH / 2
@@ -17,6 +35,11 @@ class DrawTextBox:
         self.create_page_text()
 
     def create_page_text(self):
+        """ Create the text that will be seen in the text box.
+        
+            Args:
+                self (DrawTextBox): An instance of DrawTextBox                
+        """
         self.page_text = ''
         if len(self.text_list) > 4:
             for index in range(self.cur_line, self.cur_line + 4):
@@ -27,14 +50,24 @@ class DrawTextBox:
             self.text_end = True
 
     def line_by_line(self):
+        """ Go through text line by line and set current line. 
+        
+            Args:
+                self (DrawTextBox): An instance of DrawTextBox
+        """
         self.cur_line += 1
         self.create_page_text()
     
-    def page_by_page(self):
-        self.cur_line += 4
-        self.create_page_text()
+    # def page_by_page(self):
+    #     self.cur_line += 4
+    #     self.create_page_text()
 
     def draw_text_box(self):
+        """ Draw the text box background and text.
+        
+            Args:
+                self (DrawTextBox): An instance of DrawTextBox
+        """
         arcade.draw_rectangle_filled(self.text_box_center_x, self.text_box_center_y, self.rec_width, self.rec_length, arcade.csscolor.BLACK)
         arcade.draw_point(self.text_box_center_x, self.text_box_center_y, arcade.color.BARN_RED, 5)
         if len(self.text_list) > 4:
